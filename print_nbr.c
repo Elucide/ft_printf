b7a5578c419304af9e48487b4245a14889f37508
Yoseph SCHECROUN <yschecro@e2r10p18.clusters.42paris.fr>
2022-01-16 21:22:45 +0100

    allez

--- print_nbr.c
+++
@@ -1,6 +0,0 @@
-#include "ft_printf.h"
-
-int	print_nbr(int nb)
-{
-	return (print_str(ft_itoa(nbr)));
-}
--- print_str.c
+++
@@ -1,6 +0,0 @@
-#include "ft_printf.h"
-
-int	printf_str(char *str)
-{
-	return (write(1, &str, ft_strlen(str)));
-}
