56da18f952efbe987bf09756ed3716fa181a97b5
Yoseph SCHECROUN <yschecro@e2r10p11.clusters.42paris.fr>
2022-01-28 19:09:19 +0100

    the end?

--- ft_putnbr_hexa.c
+++ ft_putnbr_hexa.c
@@ -6,7 +6,7 @@
 /*   By: yschecro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/16 20:51:31 by yschecro          #+#    #+#             */
-/*   Updated: 2022/01/27 18:35:33 by yschecro         ###   ########.fr       */
+/*   Updated: 2022/01/28 18:56:37 by yschecro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int	ft_putnbr_hexa_maj(unsigned int nb)
 	out = 0;
 	hexa = "0123456789ABCDEF";
 	if (nb > 15)
-		out += ft_putnbr_hexa(nb / 16);
+		out += ft_putnbr_hexa_maj(nb / 16);
 	out += write(1, &hexa[nb % 16], 1);
 	return (out);
 }
--- print_p.c
+++ print_p.c
@@ -6,34 +6,34 @@
 /*   By: yschecro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/16 20:47:06 by yschecro          #+#    #+#             */
-/*   Updated: 2022/01/27 18:59:18 by yschecro         ###   ########.fr       */
+/*   Updated: 2022/01/28 19:08:23 by yschecro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	ft_putadress(unsigned long nb)
+static int	ft_putadress(unsigned long long nb)
 {
-	char	*hexa;
-	long	out;
+	unsigned long long	n;
+	char				*base;
+	int					out;
 
+	base = "0123456789abcdef";
 	out = 0;
-	hexa = "0123456789abcdef";
-	if (nb > 15)
-		out += out + ft_putnbr_hexa(nb / 16);
-	out += write(1, &hexa[nb % 16], 1);
+	n = (unsigned long long) nb;
+	if (n >= 16)
+		out += ft_putadress(n / 16);
+	out += write(1, &base[n % 16], 1);
 	return (out);
 }
 
 int	print_p(va_list arg)
 {
-	long long	nb;
+	long	nb;
 
-	nb = va_arg(arg, long);
+	nb = va_arg(arg, unsigned long);
 	if (!nb)
 		return (write(1, "(nil)", 5));
-	if (nb < 0)
-		nb += 9223372036854775807;
 	write(1, "0x", 2);
 	return (ft_putadress(nb) + 2);
 }
